b18446c0c162e09f074947a5c627c36476448b88
Richmudi <mudisrichman@yahoo.com>
2023-02-16 11:22:54 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+* main - this is the header of the program
+
+* Return - this is returned to zero
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
