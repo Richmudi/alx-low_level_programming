c58f536ab01f351ed12162fbbad855aa9cbcb91c
Richmudi <mudisrichman@yahoo.com>
2023-02-16 18:49:41 -0800

    unix program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,8 @@
 *
 * Return: it should return 1
 */
-
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
