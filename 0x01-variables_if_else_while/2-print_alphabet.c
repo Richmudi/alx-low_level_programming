26e507cdd3f0613a71ec138fa44acbd30a3f0a38
Richmudi <mudisrichman@yahoo.com>
2023-02-18 17:08:54 -0800

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - All code will be in the main function
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
