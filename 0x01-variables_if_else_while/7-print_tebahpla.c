12a975d8aa764bb3347cad9935818b880f15f1a3
Richmudi <mudisrichman@yahoo.com>
2023-02-18 18:09:47 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - All code will be in the main function
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i <= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
