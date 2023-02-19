126d195b0bf836ab44ee3295de72b180806dfe0d
Richmudi <mudisrichman@yahoo.com>
2023-02-18 19:26:24 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(i + '0');
+		else
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
 	}
 	return (0);
 }
