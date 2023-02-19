6373f9f71b107f2e817ddbc42d55000bbe4db444
Richmudi <mudisrichman@yahoo.com>
2023-02-18 20:56:12 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,14 +16,14 @@ int main(void)
 		{
 			if (i < j && i != j)
 			{
-			putchar(i + '0');
-			putchar(j + '0');
-			}
+				putchar(i + '0');
+				putchar(j + '0');
 				if (i + j != 17)
 				{
-					putchar(',');
-					putchar(' ');
+				putchar(',');
+				putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
