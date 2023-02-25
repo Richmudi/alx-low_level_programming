2b923fcdeda71e0a1dac3cd6fe74c21779ea383f
Richmudi <mudisrichman@yahoo.com>
2023-02-24 20:44:11 -0800

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,7 +8,7 @@
 void positive_or_negative(int i)
 {
 	if (i < 0)
-		printf("%d is negative\n", i);
+		printf("%d is negative", i);
 	else
-		printf("%d is positive\n", i);
+		printf("%d is positive", i);
 }
